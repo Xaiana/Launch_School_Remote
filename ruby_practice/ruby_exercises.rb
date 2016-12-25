d450bb094b25f32656910c0949066cf76fa09e62
Xaiana <melisskann@gmail.com>
2016-12-24 19:27:07 -0800

    added new ruby practice directory with one new file

---
+++ ruby_practice/ruby_exercises.rb
@@ -0,0 +1,43 @@
+"Melissa" + "Keith"
+
+1250 / 1000
+1250 % 1000 / 100
+1250 % 100 / 10
+1250 % 1250
+
+films = {
+  bill_hicks_sane_man: 1989,
+  dazed_and_confused: 1993,
+  the_crow: 1994,
+  hip_hop_evolution: 2016,
+  for_the_love_of_spock: 2016,
+  the_fall: 2006,
+  the_promise: 2005 }
+
+puts films[:bill_hicks_sane_man]
+puts films[:dazed_and_confused]
+puts films[:the_crow]
+puts films[:hip_hop_evolution]
+puts films[:for_the_love_of_spock]
+puts films[:the_fall]
+puts films[:the_promise]
+
+a = [1989, 1993, 1994, 2016, 2016, 2006, 2005]
+puts a[0]
+puts a[1]
+puts a[2]
+puts a[3]
+puts a[4]
+puts a[5]
+puts a[6]
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts 5.5 * 5.5
+puts 8.8 * 8.8
+puts 4.1 * 4.1
+
+#a } was expected instead of a )
